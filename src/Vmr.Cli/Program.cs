fae98e56396621b9341036b01d84c14b6b6d8c78
melorit\rhalbaksz <rhalbaksz@melorit.hu>
2020-09-26 19:24:45 +0200

    Sln and project setup.

---
+++ src/Vmr.Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.WriteLine("Hello World!");
+
+Person p = new () 
+{ 
+    FirstName = "", 
+    LastName = "" 
+};
+
+public record Person 
+{
+    public string FirstName { get; init; } = default;
+    public string LastName { get; init; } = default;
+}
